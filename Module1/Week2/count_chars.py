8adabde37eb3501fc365bb679e4d3a68e8bf3320
An <annguyenaicomefromvietnam@gmail.com>
2024-06-16 23:30:30 +0700

    config

---
+++ Module1/Week2/count_chars.py
@@ -0,0 +1,15 @@
+def count_chars(string):
+    char_count = {}
+    for char in string:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
+
+
+string1 = 'Happiness'
+print(count_chars(string1))
+
+string2 = 'smiles'
+print(count_chars(string2))
